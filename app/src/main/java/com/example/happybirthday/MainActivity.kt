aef300ce081d846ddda2a11b3bc85f1cb719d8dc
RIAHI wael <144740463+waeleeee@users.noreply.github.com>
2024-10-08 11:49:35 +0100

    first commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,293 @@
+package com.example.tpnative
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.tpnative.ui.theme.TpNativeTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme  {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    //Tp2:
+                    LemonApp()
+                    //DiceRollerApp();
+
+                    //Tp1:
+                    //Gestionnaire();
+                    //Article();
+                    //GreetingImage(message = "Happy Birthday Rayen!", from = "From Amine", modifier = Modifier.padding(8.dp))
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LemonApp()
+}
+@Composable
+fun LemonApp() {
+    // A surface container using the 'background' color from the theme
+
+    var currentStep by remember { mutableStateOf(1) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_tree),
+                        contentDescription = stringResource(R.string.lemon_select),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(text = stringResource(R.string.lemon_tree_content_description))
+                }
+            }
+
+            2 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_squeeze),
+                        contentDescription = stringResource(R.string.lemon_squeeze),
+                        modifier = Modifier.wrapContentSize()
+                            .clickable {
+                                currentStep = 3
+                            }
+                    )
+                    Spacer(
+                        modifier = Modifier.height(
+                            32
+                                .dp
+                        )
+                    )
+                    Text(text = stringResource(R.string.lemon_select_content_description))
+                }
+            }
+            3 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_drink),
+                        contentDescription = stringResource(R.string.lemon_squeeze),
+                        modifier = Modifier.wrapContentSize()
+                            .clickable {
+                                currentStep = 4
+                            }
+                    )
+                    Spacer(
+                        modifier = Modifier.height(
+                            32
+                                .dp
+                        )
+                    )
+                    Text(text = stringResource(R.string.lemon_drink_content_description))
+                }
+            }
+            4 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_restart),
+                        contentDescription = stringResource(R.string.lemon_squeeze),
+                        modifier = Modifier.wrapContentSize()
+                            .clickable {
+                                currentStep = 1
+                            }
+                    )
+                    Spacer(
+                        modifier = Modifier.height(
+                            32
+                                .dp
+                        )
+                    )
+                    Text(text = stringResource(R.string.lemon_restart_content_description))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage();
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
+
+
+/** Tp1
+@Composable
+fun Gestionnaire(modifier: Modifier = Modifier){
+val image = painterResource(R.drawable.ic_task_completed)
+Column (
+verticalArrangement = Arrangement.Center,
+horizontalAlignment = Alignment.CenterHorizontally
+) {
+Image(painter = image, contentDescription = null)
+Text(text = "All tasks completed", fontWeight = FontWeight.Bold, modifier=Modifier.padding(
+PaddingValues(top = 24.dp)
+))
+Text(text = "Nice work!", fontSize = 16.sp)
+}
+}
+
+@Composable
+fun Article(modifier: Modifier = Modifier){
+val image = painterResource(R.drawable.bg_compose_background)
+val first_paragraph: String = "Jetpack Compose tutorial"
+val second_paragraph: String = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs."
+val third_paragraph: String = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+
+Column {
+Image(painter = image, contentDescription = null)
+Text(text = first_paragraph, fontSize = 24.sp, modifier=Modifier.padding(16.dp))
+Text(text = second_paragraph, modifier=Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+Text(text = third_paragraph, modifier=Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+}
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+Column(
+verticalArrangement = Arrangement.Center,
+modifier = modifier.padding(8.dp)
+) {
+Text(
+text = message,
+fontSize = 100.sp,
+lineHeight = 116.sp,
+textAlign = TextAlign.Center
+)
+Text(
+text = from,
+fontSize = 36.sp,
+modifier = Modifier
+.padding(16.dp)
+.align(alignment = Alignment.End)
+)
+}
+
+}
+
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+val image = painterResource(R.drawable.androidparty)
+Box(modifier){
+Image(
+painter = image,
+contentDescription = null,
+contentScale = ContentScale.Crop,
+alpha = 0.5F
+)
+GreetingText(
+message = message,
+from = from,
+modifier = Modifier.fillMaxSize())
+}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+TpNativeTheme {
+GreetingImage(
+message = "Happy Birthday Sam!",
+from = "From Emma"
+)
+}
+}
+ **/
